4272c832b6379f830d23ad030d700342dda04c1b
Brian Clifton <brian@clifton.me>
2019-08-05 09:54:00 -0700

    Initial implementation of P3A infobar

--- chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chromium_src/chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -1,3 +1,8 @@
+/* Copyright (c) 2019 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "chrome/browser/ui/startup/google_api_keys_infobar_delegate.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 
@@ -5,11 +10,11 @@
 
 class BraveGoogleKeysInfoBarDelegate {
  public:
-   static void Create(InfoBarService* infobar_service) {
-     // lulz
-   }
+  static void Create(InfoBarService* infobar_service) {
+    // lulz
+  }
 };
 
-#include "../../../../../../chrome/browser/ui/startup/startup_browser_creator_impl.cc"
+#include "../../../../../../chrome/browser/ui/startup/startup_browser_creator_impl.cc" // NOLINT
 
 #undef GoogleApiKeysInfoBarDelegate
