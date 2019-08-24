a88efe70142249c01a5832719147bab78d0f85e7
Jocelyn Liu <yrliou@gmail.com>
2019-08-23 23:18:09 -0700

    Hook into chrome_extensions_browser_client to use parent profile's extensions in Tor profile.

--- chromium_src/chrome/browser/extensions/chrome_extensions_browser_client.cc
+++ chromium_src/chrome/browser/extensions/chrome_extensions_browser_client.cc
@@ -5,11 +5,19 @@
 
 #include "brave/browser/extensions/api/brave_extensions_api_client.h"
 #include "brave/browser/extensions/updater/brave_update_client_config.h"
+#include "brave/browser/profiles/profile_util.h"
 #include "chrome/browser/extensions/chrome_extensions_browser_client.h"
 #include "chrome/browser/extensions/updater/chrome_update_client_config.h"
+
+#define BRAVE_GET_ORIGINAL_CONTEXT_             \
+  if (brave::IsTorProfile(context)) {           \
+    return brave::GetTorParentProfile(context); \
+  }
+// define BRAVE_GET_ORIGINAL_CONTEXT_
 
 #define ChromeUpdateClientConfig BraveUpdateClientConfig
 #define ChromeExtensionsAPIClient BraveExtensionsAPIClient
 #include "../../../../../chrome/browser/extensions/chrome_extensions_browser_client.cc"  // NOLINT
 #undef ChromeUpdateClientConfig
 #undef ChromeExtensionsAPIClient
+#undef BRAVE_GET_ORIGINAL_CONTEXT_
